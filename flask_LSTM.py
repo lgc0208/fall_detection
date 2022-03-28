da6d0742eb8e2820844382a6201f76aa8128b910
lgc0208 <lgc0208@foxmail.com>
2022-03-28 10:22:24 +0800

    2022-3-28：对代码规范进行修正

--- flask_LSTM.py
+++ flask_LSTM.py
@@ -1,7 +1,7 @@
 """
     File Name:          flask_LSTM.py
     Author:             LIN Guocheng
-    Version:            1.0.2
+    Version:            1.0.3
     Description:        使用 Flask 进行模型部署
     History:            
         1.  Date:           2022-1-16
@@ -13,10 +13,15 @@
         3.  Date:           2022-1-21
             Author:         LIN Guocheng
             Modification:   修改了host地址和预测返回值
+        4.  Date:           2022-1-26
+            Author:         LIN Guocheng
+            Modification:   针对可能产生的错误 unexpected EOF while parsing 进行修正
 """
 
 import flask
 import numpy as np
+import logging
+from gevent import pywsgi
 from tensorflow.keras.models import load_model
 
 host = '0.0.0.0'
@@ -27,44 +32,64 @@ app = flask.Flask(__name__)
 # 加载模型
 model = load_model("model/LSTM.h5")
 
+
+@app.before_request
+def enable_form_raw_cache():
+    if flask.request.path.startswith('/predict'):
+        if flask.request.content_length > 1024 * 1024:  # 1mb
+            flask.abort(413)  # Payload too large
+        app.logger.info(flask.request.headers)
+        app.logger.info(flask.request.data)
+
+
 # 初始界面
 @app.route("/")
 def index():
     return "<h1> 服务器正在正常运行 <h1>"
 
+
 # 预测界面
 @app.route("/predict", methods=["GET", "POST"])
-
 def predict():
-    data = {"success" : False}
-    data = {"result" : 0}
-    
+    data = {"success": False, "result": 0}
+
     params = flask.json
-    if(params == None):
+
+    if params is None:
         params = flask.request.form.getlist("")
-        
+
     # 接收到输入参数，返回预测值
-    if(params != None):
-        print("##################/n")
+    if params is not None:
+        app.logger.info("\n")
         test_data = flask.request.args.getlist("testData")
-        test_data = eval(str(test_data).replace("\'", ""))
-        test_data = np.array(test_data)
-        # 升维
-        test_data = test_data[None,:,:]
-        print("收到预测请求\n")
-        y_predict = model.predict(test_data)  
-        print("预测结果为：")
-        if(y_predict > 0.5):
-            data["result"] = 1 # "出现摔倒"
-        else:
-           data["result"] = 0 # "无事发生"
-        #data["prediction"] = str(y_predict)
-        print(data["result"])
-        data["success"] = True
-        
+        if test_data is not None:
+            test_data = eval(str(test_data).replace("\'", ""))
+            test_data = np.array(test_data)
+            # 升维
+            test_data = test_data[None, :, :]
+            app.logger.info("收到预测请求\n")
+            y_predict = model.predict(test_data)
+            app.logger.info("预测结果为：")
+            if y_predict > 0.5:
+                data["result"] = 1  # "出现摔倒"
+            else:
+                data["result"] = 0  # "无事发生"
+            app.logger.info(data["result"])
+            app.logger.info("\n")
+            data["success"] = True
+
         # 返回 json
         return flask.jsonify(data)
-    
+
+
 # 启动 flask
 if __name__ == '__main__':
-    app.run(debug=False, host=host)
+    app.debug = True
+    logging.disable_existing_loggers = False
+    handler = logging.FileHandler('logs/flask.log', encoding='UTF-8')
+    handler.setLevel(logging.DEBUG)
+    logging_format = logging.Formatter("%(asctime)s flask %(levelname)s %(message)s")
+    handler.setFormatter(logging_format)
+    app.logger.addHandler(handler)
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, log=app.logger)
+    server.serve_forever()
